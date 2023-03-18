3e53bfc3f42fd51091114bc8a5264542170aaeed
giulliano <giullianomendes033@gmail.com>
2023-03-18 12:52:22 -0300

    site atualizado

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,15 @@ module.exports = {
         'blink': {
           '0%, 100%': { opacity: 1 },
           '50%': { opacity: 0 },
-        }
+        },
+        'fade-top': {
+          '0%': { transform: 'translateY(20px)' },
+          '100%': { transform: 'translateY(0)' },
+        },
       },
       animation: {
         'blink': 'blink 1.7s linear infinite',
+        'fade-top': 'fade-top 0.5s ease-out',
       },
       screens: {
         'xs': '320px',
